aa7ab5200493b4bcad599f4f40160dac4d88c261
Adrien Clerbois <adrien.clerbois@outlook.com>
2017-07-29 15:01:35 +0200

    Add back office

---
+++ sources/bo/PepperMap.BackOffice/Controllers/LocationsController.cs
@@ -0,0 +1,160 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PepperMap.BackOffice.Data;
+using PepperMap.BackOffice.Data.Models;
+
+namespace PepperMap.BackOffice.Controllers
+{
+    public class LocationsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LocationsController(ApplicationDbContext context)
+        {
+            _context = context;    
+        }
+
+        // GET: Locations
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Locations.Include(l => l.Route);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Locations/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var location = await _context.Locations
+                .Include(l => l.Route)
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (location == null)
+            {
+                return NotFound();
+            }
+
+            return View(location);
+        }
+
+        // GET: Locations/Create
+        public IActionResult Create()
+        {
+            ViewData["RouteId"] = new SelectList(_context.Routes, "Id", "Id");
+            return View();
+        }
+
+        // POST: Locations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,RouteId,Name")] Location location)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(location);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewData["RouteId"] = new SelectList(_context.Routes, "Id", "Id", location.RouteId);
+            return View(location);
+        }
+
+        // GET: Locations/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var location = await _context.Locations.SingleOrDefaultAsync(m => m.Id == id);
+            if (location == null)
+            {
+                return NotFound();
+            }
+            ViewData["RouteId"] = new SelectList(_context.Routes, "Id", "Id", location.RouteId);
+            return View(location);
+        }
+
+        // POST: Locations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,RouteId,Name")] Location location)
+        {
+            if (id != location.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(location);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LocationExists(location.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            ViewData["RouteId"] = new SelectList(_context.Routes, "Id", "Id", location.RouteId);
+            return View(location);
+        }
+
+        // GET: Locations/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var location = await _context.Locations
+                .Include(l => l.Route)
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (location == null)
+            {
+                return NotFound();
+            }
+
+            return View(location);
+        }
+
+        // POST: Locations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var location = await _context.Locations.SingleOrDefaultAsync(m => m.Id == id);
+            _context.Locations.Remove(location);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        private bool LocationExists(int id)
+        {
+            return _context.Locations.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ sources/bo/PepperMap.BackOffice/Controllers/PeopleController.cs
@@ -0,0 +1,160 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PepperMap.BackOffice.Data;
+using PepperMap.BackOffice.Data.Models;
+
+namespace PepperMap.BackOffice.Controllers
+{
+    public class PeopleController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PeopleController(ApplicationDbContext context)
+        {
+            _context = context;    
+        }
+
+        // GET: People
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.People.Include(p => p.Location);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: People/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.People
+                .Include(p => p.Location)
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return View(person);
+        }
+
+        // GET: People/Create
+        public IActionResult Create()
+        {
+            ViewData["LocationId"] = new SelectList(_context.Locations, "Id", "Id");
+            return View();
+        }
+
+        // POST: People/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,LocationId,Lastname,Firstname,Title,Service,Flag")] Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(person);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewData["LocationId"] = new SelectList(_context.Locations, "Id", "Id", person.LocationId);
+            return View(person);
+        }
+
+        // GET: People/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.People.SingleOrDefaultAsync(m => m.Id == id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            ViewData["LocationId"] = new SelectList(_context.Locations, "Id", "Id", person.LocationId);
+            return View(person);
+        }
+
+        // POST: People/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,LocationId,Lastname,Firstname,Title,Service,Flag")] Person person)
+        {
+            if (id != person.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(person);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PersonExists(person.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            ViewData["LocationId"] = new SelectList(_context.Locations, "Id", "Id", person.LocationId);
+            return View(person);
+        }
+
+        // GET: People/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.People
+                .Include(p => p.Location)
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return View(person);
+        }
+
+        // POST: People/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var person = await _context.People.SingleOrDefaultAsync(m => m.Id == id);
+            _context.People.Remove(person);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        private bool PersonExists(int id)
+        {
+            return _context.People.Any(e => e.Id == id);
+        }
+    }
+}
